716a1902abd15404ad283a5ac5ffe72dbb123438
Argo-AscioTech <argo@live.ca>
2024-04-05 11:22:27 +0800

    test: 增加单元测试

---
+++ test/TrigerTest.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Longbow.Tasks.Test;
+
+public class TrigerTest
+{
+    [Fact]
+    public void Run_Ok()
+    {
+        TaskServicesManager.Init();
+
+        var autoReset = new AutoResetEvent(false);
+        var count = 0;
+        var scheduler1 = TaskServicesManager.GetOrAdd("test-run", (provider, token) =>
+        {
+            count++;
+            autoReset.Set();
+            return Task.CompletedTask;
+        });
+        autoReset.WaitOne();
+
+        scheduler1.Run();
+        autoReset.WaitOne();
+        Assert.Equal(2, count);
+    }
+}
